ce80ca56fe1e1460875e4ba01dbb9925400b8f67
[symonkipkemei] <[symonkipkemei@gmail.com]>
2022-01-10 10:28:05 +0300

    using print

--- symon-kiplelgo.py
+++ symon-kiplelgo.py
@@ -1,2 +1,5 @@
 #message to the world
 print("i just learnt how to type faster than an average speed")
+
+print("I feel i have been extremely productive today")
+print("All the best as you continue to practice on python programming")
